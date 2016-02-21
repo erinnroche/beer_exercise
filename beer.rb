0d49e7039a956d25f24915b5c3e3f06e286f833f
erinnroche <erinnroche@gmail.com>
2016-02-21 14:22:18 -0500

    have a beer

--- beer.rb
+++ beer.rb
@@ -1,5 +1,22 @@
 require 'pry'
 
+  puts("what is your age?")
+  user_age = gets.strip
+  if user_age >= ('21')
+    puts("Yay, have a beer!")
+  elsif user_age < ('21')
+    puts("Nay, don't have a beer")
+  end
+
+
+
+
+
+
+
+
+
+
 #write a function that accepts a variable: age
 #if age is greater than 21, display "Yay, have a beer!"
 #if age is less than 21, display "Nay, don't have a beer!"
